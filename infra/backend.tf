5e995cb04f4ea024692211232ca2f9d220e32cc4
Harjas Singh <harjas.singh.dev@gmail.com>
2026-07-11 19:03:03 +0530

    commented the prerequisites in backend.tf file

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
-Prerequisites:
-  1. Create S3 bucket:  aws s3api create-bucket --bucket docpipeline-terraform-state --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
-  2. Enable versioning: aws s3api put-bucket-versioning --bucket docpipeline-terraform-state --versioning-configuration Status=Enabled
-  3. Create DynamoDB:   aws dynamodb create-table --table-name docpipeline-terraform-locks --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --billing-mode PAY_PER_REQUEST --region ap-south-1
+# Prerequisites:
+#   1. Create S3 bucket:  aws s3api create-bucket --bucket docpipeline-terraform-state --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
+#   2. Enable versioning: aws s3api put-bucket-versioning --bucket docpipeline-terraform-state --versioning-configuration Status=Enabled
+#   3. Create DynamoDB:   aws dynamodb create-table --table-name docpipeline-terraform-locks --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --billing-mode PAY_PER_REQUEST --region ap-south-1
 
 terraform {
   backend "s3" {
